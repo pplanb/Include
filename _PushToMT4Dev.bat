1bc527c285b9406f28ae945cdb9f4da49bcf3679
pplanb <rolandhablutzel@hotmail.com>
2018-10-01 16:15:06 +0100

    updated path

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -4,8 +4,8 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\Include"
-set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
+set SOURCE_DIR="D:\TradingRepos\Include"
+set DEST_DIR="D:\FxPro - Terminal2\MQL4\Include"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mqh
 
--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,16 +4,12 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\Include"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Include"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Include"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Include"
+set SOURCE_DIR="D:\TradingRepos\Include"
+set DEST_DIR1="D:\FxPro - Terminal1\MQL4\Include"
+set DEST_DIR2="D:\FxPro - Terminal2\MQL4\Include"
+set DEST_DIR3="D:\FxPro - Terminal3\MQL4\Include"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mqh
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mqh
 ROBOCOPY %SOURCE_DIR% %DEST_DIR3% *.mqh
-ROBOCOPY %SOURCE_DIR% %DEST_DIR4% *.mqh
-
-
 
